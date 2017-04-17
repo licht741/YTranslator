492cf12cf0e295f043bef42bdb34709569361030
Nikita Nezhevskii <empthynickname@gmail.com>
2017-04-18 00:43:13 +0300

    Added search and back arrow on select languages toolbar

--- app/src/main/java/com/licht/ytranslator/ui/LanguageSelectView/LanguageListAdapter.java
+++ app/src/main/java/com/licht/ytranslator/ui/LanguageSelectView/LanguageListAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,16 +16,24 @@ import com.licht.ytranslator.R;
 import java.util.ArrayList;
 import java.util.List;
 
-class LanguageListAdapter extends RecyclerView.Adapter<LanguageListAdapter.LanguageViewHolder> {
+class LanguageListAdapter extends RecyclerView.Adapter<LanguageListAdapter.LanguageViewHolder>
+        implements Filterable{
 
     private final ISelectLanguageView view;
-    private final List<String> mLanguages;
+
+    private final List<String> mLanguages = new ArrayList<>();
+    private final List<String> mFilteredItems = new ArrayList<>();
+
     private final String currentSelectedLanguage;
     private Context context;
 
+    private Filter filter;
+
     LanguageListAdapter(ISelectLanguageView view, List<String> languages, String currentSelectedLanguage) {
         this.view = view;
-        this.mLanguages = languages;
+        mLanguages.addAll(languages);
+        mFilteredItems.addAll(languages);
+
         this.currentSelectedLanguage = currentSelectedLanguage;
     }
 
@@ -39,7 +49,7 @@ class LanguageListAdapter extends RecyclerView.Adapter<LanguageListAdapter.Langu
 
     @Override
     public void onBindViewHolder(LanguageViewHolder holder, int position) {
-        final String lang = mLanguages.get(position);
+        final String lang = mFilteredItems.get(position);
 
         holder.mLanguage.setText(lang);
 
@@ -58,8 +68,9 @@ class LanguageListAdapter extends RecyclerView.Adapter<LanguageListAdapter.Langu
 
     @Override
     public int getItemCount() {
-        return mLanguages.size();
+        return mFilteredItems.size();
     }
+
 
     static class LanguageViewHolder extends RecyclerView.ViewHolder {
         final TextView mLanguage;
@@ -67,10 +78,56 @@ class LanguageListAdapter extends RecyclerView.Adapter<LanguageListAdapter.Langu
 
         LanguageViewHolder(View itemView) {
             super(itemView);
-
             mLanguage = (TextView) itemView.findViewById(R.id.tv_language);
             mIsSelected = (ImageView) itemView.findViewById(R.id.iv_is_selected);
         }
+    }
 
+    @Override
+    public Filter getFilter() {
+        if (filter == null)
+            filter = new LanguageFilter(this, mLanguages);
+
+        return filter;
+    }
+
+    private static class LanguageFilter extends Filter {
+        private final LanguageListAdapter adapter;
+        private final List<String> originalList;
+        private final List<String> filteredList;
+
+        LanguageFilter(LanguageListAdapter adapter,
+                              List<String> originalList) {
+            this.adapter = adapter;
+            this.originalList = originalList;
+            this.filteredList = new ArrayList<>();
+        }
+
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            filteredList.clear();
+            final FilterResults results = new FilterResults();
+
+            if (constraint.length() > 0) {
+                final String pattern = constraint.toString().toLowerCase().trim();
+                for (final String language : originalList)
+                    if (language.toLowerCase().contains(pattern))
+                        filteredList.add(language);
+
+            } else
+                filteredList.addAll(originalList);
+
+            results.values = filteredList;
+            results.count = filteredList.size();
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            adapter.mFilteredItems.clear();
+            adapter.mFilteredItems.addAll((List<String>) results.values);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/licht/ytranslator/ui/LanguageSelectView/SelectLanguageActivity.java
+++ app/src/main/java/com/licht/ytranslator/ui/LanguageSelectView/SelectLanguageActivity.java
@@ -1,12 +1,17 @@
 package com.licht.ytranslator.ui.LanguageSelectView;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.EditText;
 
 import com.licht.ytranslator.R;
 
@@ -16,13 +21,18 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public class SelectLanguageActivity extends AppCompatActivity implements ISelectLanguageView {
+public class SelectLanguageActivity extends AppCompatActivity
+        implements ISelectLanguageView, SearchView.OnQueryTextListener {
     public static final String AVAILABLE_LANGUAGE_LIST = "LANGUAGE_LIST";
     public static final String SELECTED_LANGUAGE = "SELECTED_LANGUAGE";
     public static final String RESULT_LANGUAGE = "RESULT_LANGUAGE";
 
     @BindView(R.id.rv_languages_list) RecyclerView mRecyclerView;
     private Unbinder unbinder;
+
+    private LanguageListAdapter mAdapter = null;
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +51,7 @@ public class SelectLanguageActivity extends AppCompatActivity implements ISelect
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        LanguageListAdapter mAdapter = new LanguageListAdapter(this, languagesList, currentLanguage);
+        mAdapter = new LanguageListAdapter(this, languagesList, currentLanguage);
         mRecyclerView.setAdapter(mAdapter);
     }
 
@@ -49,6 +59,28 @@ public class SelectLanguageActivity extends AppCompatActivity implements ISelect
     protected void onStart() {
         super.onStart();
         getSupportActionBar().setTitle(getString(R.string.select_language));
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_select_language, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(this);
+
+        EditText searchEditText = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchEditText.setTextColor(ContextCompat.getColor(this, android.R.color.white));
+        searchEditText.setHintTextColor(ContextCompat.getColor(this, android.R.color.white));
+
+        return true;
     }
 
     @Override
@@ -65,9 +97,28 @@ public class SelectLanguageActivity extends AppCompatActivity implements ISelect
         unbinder.unbind();
     }
 
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        mAdapter.getFilter().filter(newText);
+        return false;
+    }
+
     private void initUI() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setTitleTextColor(ContextCompat.getColor(this, android.R.color.white));
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
